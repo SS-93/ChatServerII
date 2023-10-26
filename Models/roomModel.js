a06db45192dc2fd35a9e981efb217aa5e3a11c8e
SS-93 <Sesughtor@gmail.com>
2023-10-25 17:07:09 -0700

    recent update

--- Models/roomModel.js
+++ Models/roomModel.js
@@ -1,4 +1,4 @@
- SS
+ 
 const mongoose = require ('mongoose');
 
 const RoomSchema = new mongoose.Schema({
@@ -10,11 +10,11 @@ title: {
  description: {
     type: String,
     required: false,
- },
- messages: {
- type: Array
- },
- ownerId: Object.id  
+ }
+//  messages: {
+//  type: Array
+//  },
+//  ownerId: Object.id  
  
 });
 
@@ -23,29 +23,6 @@ title: {
 
 
 
-module.exports = mongoose.model ('User', UserSchema );
+module.exports = mongoose.model ('Room', RoomSchema );
 
-const mongose = require('mongoose');
 
-const RoomSchema = new mongoose.Schema({
-title: {
-    type: String, 
-    required: true,
-    unique: true
-},
-discription: {
-    type: String,
-    required: false, 
-},
-messages: {
-    type: Array
-},
-ownerID: {
-    type: Schema.types.ObjectId,
-    ref: "User"
-}
-
-});
-
-module.exports = mongoose.model('User', UserSchema);
- develop
--- index.js
+++ index.js
@@ -2,8 +2,13 @@ require('dotenv').config();
 const express = require ('express');
 const app = express();
 
+
 const mongoose = require('mongoose')
 
+const rooms = require('./Controllers/roomController');
+
+// const userModel = require ('./Models/userModel')
+const users = require('./Contollers/userController')
 
 const {PORT, MONGO } = process.env;
 
@@ -13,10 +18,12 @@ const db = mongoose.connection;
 
 db.once('open', () => console.log(`Connected to: ${MONGO}`));
 app.use(express.json());
+app.use(require('cors')())
 
+app.use('/rooms', rooms)
+app.use('/user', users)
 
-
-app.get('/testing', (req, res) => {
+app.get('/test', (req, res) => {
     res.status(200).json({ message: "Server is accessible", port: process.env.PORT })
 });
 
